9b6abaac1215264dda7a84f471054a2be3e08b7f
Kyle Nally <kylegnally@gmail.com>
2018-01-03 09:12:42 -0500

    Initial commit. Blank project.

---
+++ android/src/com/kylenally/islandquest/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kylenally.islandquest;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kylenally.islandquest.IslandQuest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new IslandQuest(), config);
+	}
+}
